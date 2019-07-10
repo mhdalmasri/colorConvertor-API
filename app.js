a2c8a5dba25842321b1612ffc80d9872d3ae8a5a
mhdalmasri <m.almasri@outlook.de>
2019-07-10 14:49:25 +0200

    error fixed

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var convert = require("color-convert");
-var fs = require('fs');
-
+var fs = require("fs");
 
 var app = express();
 
@@ -16,32 +15,30 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 // Logic:.......
-// myLogger
-app.use(function (req, res, next) {
-  console.log('Request URL:', req.originalUrl)
-  next()
-}, function (req, res, next) {
-  console.log('Request Type:', req.method)
-  next()
-})
+// myLogger middleware
+app.use(
+  function(req, res, next) {
+    console.log("Request URL:", req.originalUrl);
+    next();
+  },
+  function(req, res, next) {
+    console.log("Request Type:", req.method);
+    next();
+  }
+);
+// error middleware
+app.use(function(req, res, next) {
+  if (req.query.color) {
+    console.log("query parameter color available");
+    next();
+  } else {
+    console.log("Error: Missing query parameter color");
+    res.status(500).send("Error: Missing query parameter color");
+  }
+  next();
+});
 
-var requestTime = function (req, res, next) {
-  req.requestTime = Date.now()
-  next()
-};
-app.use(requestTime);
-
-app.use('/convert/keywordtorgb',function (req, res, next) {
-  if (req.query.color){
-    next()
-   }
-  else
-  console.log('Error: Missing query parameter color')
-  res.status(500).send('Error: Missing query parameter color')})
-  
-
-
-app.get("/", (req, res) => res.send(`Hi to Express App .. Date now is : ${req.requestTime}`));
+app.get("/", (req, res) => res.send("Hi to Express App"));
 
 app.get("/convert/rgbtohsl", (req, res) =>
   res.send(convert.rgb.hsl(req.query.color))
@@ -51,16 +48,6 @@ app.get("/convert/keywordtorgb", (req, res) =>
   res.send(convert.keyword.rgb(req.query.color))
 );
 
-
-// fs.writeFile("./statistics.txt", 'req.query.color', (err) => {
-//   if (err) console.log(err);
-//   console.log("Successfully Written to File.");
-// });
-
-// app.get("/statistic", (req, res) =>
-//   res.send(statistics)
-//   );
-
 app.get("/convert/rgbtohex", (req, res) =>
   res.send(convert.rgb.hex(req.query.color))
 );
@@ -69,23 +56,4 @@ app.get("/convert/hextorgb", (req, res) =>
   res.send(convert.hex.rgb(req.query.color))
 );
 
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.send("error");
-});
-
 module.exports = app;
-
